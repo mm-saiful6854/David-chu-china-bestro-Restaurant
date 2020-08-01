19151fc7eaf696a96a36e8b03f98e9b25d60ba44
mm-saiful6854 <cse.saiful2119@gmail.com>
2020-08-01 09:15:24 +0600

    module4 completed

---
+++ site/javascript/js/script.js
@@ -0,0 +1,73 @@
+// // string concatenation
+
+// // var s ="hello";
+// // s+=" world!";
+// // console.log(s);
+
+
+// // // basic math operation
+// // var x = (5+3)/2+9/3;
+// // console.log(x);
+// // var d = undefined/5;
+// // console.log(d);
+
+
+
+// // handling default value
+
+// // function A(x){
+// //     x = x || "default";
+// //     console.log("somethings " + x);
+// // };
+
+// // A();
+
+// // passing by value
+
+// // function passvalue(prim){
+// //     console.log(prim);
+// //     prim.value=5;
+// //     console.log(prim);
+// // };
+
+// // prim = new Object();
+// // prim.name="number";
+// // prim.value=100;
+
+// // passvalue(prim);
+
+// // console.log(prim);
+
+// function Circle (radius){
+//     this.radius = radius;
+// }
+
+// Circle.prototype.getarea = function (){
+//     console.log(this);
+//     return Math.PI * Math.pow(this.radius, 2);
+// };
+
+
+// function Square(arm){
+//     this.arm= arm;
+// };
+
+// function dummy(){
+//     return "saiful";
+// }
+
+// var c = new Circle(10);
+// var s = new Square(4);
+
+// console.log(c.getarea().toString());
+// console.log(s);
+// console.log(dummy.toString());
+
+
+
+var st = "saiful";
+
+
+for(var i=0;i<st.length;i++){
+   
+}
